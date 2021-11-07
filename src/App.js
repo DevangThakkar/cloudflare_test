7f75737d77f4ccf1fbb981e4e72174629601808d
devang <devang01121995@gmail.com>
2021-11-07 00:53:47 -0400

    try requesting input

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Router } from "@reach/router";
 
 import Posts from './components/posts'
 import New from './components/new'
+import Submitted from './components/submitted'
 
 function App() {
   return (
