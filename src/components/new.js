e94ea7f42ba6bf76eb73ff7f841aa43b146a7169
devang <devang01121995@gmail.com>
2021-11-07 00:22:38 -0400

    try requesting input

--- src/components/new.js
+++ src/components/new.js
@@ -18,6 +18,7 @@ export default class New extends React.Component {
         body: JSON.stringify({'title': this.title.value, 'username': this.username.value, 'content': this.content.value})
       });
     })();
+    window.location="/new";
   };
 
   render() {
